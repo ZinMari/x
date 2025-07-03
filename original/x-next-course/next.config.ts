7fc21bfdbd154c4c2b235ceaa46f216c1678ac83
M <mari.v.zinkevich@gmail.com>
2025-07-03 19:50:24 +0300

    add original folder

---
+++ original/x-next-course/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+	reactStrictMode: true,
+	env: {
+		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+	},
+	images: {
+		domains: ['fakestoreapi.com']
+	}
+}
+
+export default nextConfig
