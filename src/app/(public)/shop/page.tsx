ffe9581192274e17a7d19e9aa3de47d708e7d1f5
Mari Zin <mari.v.zinkevich@gmail.com>
2025-07-16 18:16:05 +0300

    feat: create shop

---
+++ src/app/(public)/shop/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Shop"
+}
+
+export default function ShopSSRPage(){
+    return <div>SSR</div>
+}
--- src/config/pages.config.ts
+++ src/config/pages.config.ts
@@ -2,5 +2,8 @@ export const PAGES ={
     HOME: '/',
     EXPLORE: '/explore',
     PROFILE_FAKE: '/profile-fake',
-    PROFILE: (username: string)=>`/u/${username}`
+    SHOP: '/shop',
+    SSG: '/shop/ssg',
+    ISR: '/shop/isr',
+    PROFILE: (username: string)=>`/u/${username}`,
 }
