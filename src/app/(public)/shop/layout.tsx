6587cd5941231642ee7d0083d334ff9be4988b1e
Mari Zin <mari.v.zinkevich@gmail.com>
2025-08-25 18:18:50 +0300

    feat: add dynamic import to ssr page

--- src/app/(public)/shop/layout.tsx
+++ src/app/(public)/shop/layout.tsx
@@ -1,12 +1,19 @@
-import type { PropsWithChildren } from "react";
-import { ShopMenu } from "./ShopMenu";
+"use client";
 
-export default function Layout({children}:PropsWithChildren<unknown>){
-    return (
-        <div className="w-full">
-            <h1 className="text-3xl font-bold mb-6">Shop</h1>
-            <ShopMenu/>
-            {children}
-        </div>
-    )
+import type { PropsWithChildren } from "react";
+import dynamic from "next/dynamic";
+
+const DynamicShopMenu = dynamic(
+  () => import("./ShopMenu").then((mod) => mod.ShopMenu),
+  { ssr: false, loading: () => <div>Loading...</div> }
+);
+
+export default function Layout({ children }: PropsWithChildren<unknown>) {
+  return (
+    <div className="w-full">
+      <h1 className="text-3xl font-bold mb-6">Shop</h1>
+      <DynamicShopMenu />
+      {children}
+    </div>
+  );
 }
