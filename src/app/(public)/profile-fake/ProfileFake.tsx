a3eb3ec0cf7f0e17ba8a1e7119e6d71ded0c62b6
Mari Zin <mari.v.zinkevich@gmail.com>
2025-08-25 18:29:03 +0300

    feat: add lazyLoading to ProfileButton on ProfileFake page

--- src/app/(public)/profile-fake/ProfileFake.tsx
+++ src/app/(public)/profile-fake/ProfileFake.tsx
@@ -1,11 +1,15 @@
-import { ProfileButton } from "./ProfileButton";
+import dynamic from "next/dynamic";
+
+const DynamicProfileButton = dynamic(() =>
+  import("./ProfileButton").then((mod) => mod.ProfileButton)
+);
 
 export function ProfileFake() {
   return (
     <div>
       <h1 className="text-3xl font-bold mb-6">Profile Fake</h1>
 
-      <ProfileButton />
+      <DynamicProfileButton />
     </div>
   );
 }
