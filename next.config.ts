1955edd040bde45fbb4357701900ddea2219df85
Mari Zin <mari.v.zinkevich@gmail.com>
2025-08-25 18:17:05 +0300

    feat: update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  //переименование env
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   images: {
-    remotePatterns: [new URL('https://fakestoreapi.com/img/**')],
-  }
+    remotePatterns: [new URL("https://fakestoreapi.com/img/**")],
+  },
 };
 
 export default nextConfig;
