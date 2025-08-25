07cb26dc59a9fe698d9af54b8a0d3967247d109b
Mari Zin <mari.v.zinkevich@gmail.com>
2025-08-25 18:23:43 +0300

    feat: create ProfileButton conponent

---
+++ src/app/(public)/profile-fake/ProfileButton.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { PAGES } from "@/config/pages.config";
+
+export function ProfileButton() {
+  const router = useRouter();
+  return <button onClick={() => router.push(PAGES.HOME)}>Go to Home</button>;
+}
--- src/app/(public)/profile-fake/ProfileFake.tsx
+++ src/app/(public)/profile-fake/ProfileFake.tsx
@@ -1,11 +1,11 @@
-'use client'
-import { useRouter } from "next/navigation"
-import { PAGES } from "@/config/pages.config";
+import { ProfileButton } from "./ProfileButton";
 
-export function ProfileFake(){
-    const router = useRouter();
-    return <div>
-    <h1 className="text-3xl font-bold mb-6">Profile Fake</h1>
-    <button onClick={()=>router.push(PAGES.HOME)}>Go to Home</button>
-</div>
+export function ProfileFake() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Profile Fake</h1>
+
+      <ProfileButton />
+    </div>
+  );
 }
